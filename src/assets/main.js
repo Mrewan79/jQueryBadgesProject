8923355f2fd95641d5354844a57a7c187e628957
Yoosername79 <legoflamb79@gmail.com>
2017-10-03 11:35:20 +1100

    Project finished

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,16 @@
-$(function() {
-
-  // your code will go here
-
-});
+  $(function() {
+  // your code will go here. Add ajax call to codeschool badges;  
+	 $.ajax({
+      url: 'https://www.codeschool.com/users/1404115.json',
+      dataType: 'jsonp',
+      success: function(response) {
+          for (var i = 0; i < response.courses.completed.length; i++) {
+          var title = response.courses.completed[i].title;
+          var url = response.courses.completed[i].url;
+          var badge = response.courses.completed[i].badge;
+          $("#badges").append('<div class="course"><h3>' + title + '</h3><img src="' + badge + '"><a href="' + url + '" target="_blank" class="btn btn-primary">See Course</a></div>');
+        }
+      }
+    });
+  
+  });
